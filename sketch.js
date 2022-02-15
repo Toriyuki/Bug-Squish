357fdb091f713dd8f5e9c7a71e48797ba42af4dd
Toriyuki <51471099+Toriyuki@users.noreply.github.com>
2022-02-15 16:39:34 -0600

    Added Difficulties

--- sketch.js
+++ sketch.js
@@ -4,9 +4,13 @@ let bugArray = [];
 let STARTING_BUG_COUNT = 30;
 let xMax = 800;
 let yMax = 600;
+let state = "start";
+let speed = .03;
+let difficulty;
 
 let timer;
 let maxTime = 30;
+let startTime = 0;
 let score = 0;
 let totalClick = 0;
 let successClick = 0;
@@ -26,19 +30,58 @@ function setup() {
 }
 
 function draw() {
-  rect(0, 0, 800, 800);
+  rect(0, 0, xMax, yMax);
   timer = round(millis()/1000);
-  countdown = maxTime - timer; 
-  if(countdown > 0) {
-    gameScreen();
+  countdown = maxTime - timer + startTime; 
+  if(state == "start") {
+    startScreen();
+    startTime = round(millis()/1000);
   }
-  else {
+  else if(state == "game") {
+    gameScreen();
+    if(countdown <= 0) {
+      state = "end";
+    }
+  }
+  else if (state == "end"){
     while(bugArray.length > 0) {
       bugArray.pop();
     }
     endScreen();
   }
 
+}
+
+function startScreen() {
+  textSize(100);
+  text("Bug Squish", 100, 100);
+  textSize(80);
+  rect(150, 150, 400, 100);
+  text("Easy", 250, 225);
+  rect(150, 300, 400, 100);
+  text("Normal", 215, 380);
+  rect(150, 450, 400, 100);
+  text("Hard", 250, 525);
+  if(mouseIsPressed) {
+    if((mouseX > 150) && (mouseX < 550) && (mouseY > 150) && (mouseY < 250)) {
+      difficulty = "Easy";
+      base_speed = .75;
+      increase_speed = .015;
+      state = "game";
+    }
+    else if((mouseX > 150) && (mouseX < 550) && (mouseY > 300) && (mouseY < 400)) {
+      difficulty = "Normal";
+      base_speed = 1;
+      increase_speed = .03;
+      state = "game";
+    }
+    else if((mouseX > 150) && (mouseX < 550) && (mouseY > 450) && (mouseY < 550)) {
+      difficulty = "Hard";
+      base_speed = 1.15;
+      increase_speed = .035;
+      state = "game";
+    }
+  }
 }
 
 function gameScreen() {
@@ -55,14 +98,27 @@ function gameScreen() {
     acc = parseFloat((successClick / totalClick) * 100).toFixed(2);
   }
   text(acc + "%", 680, 30);
-  line(0, 40, xMax, 40);
+  line(0, 40, xMax, 40);  
   for(var i = 0; i < bugArray.length; i++) {
     bugArray[i].draw();
+    if((difficulty == "Hard") && (!bugArray[i].hasChanged()) && (countdown % 3 == 0)) {
+      setRandomMovement(bugArray[i]);
+      bugArray[i].setChanage(true);
+    }
+    else if (countdown % 3 == 1) {
+      bugArray[i].setChanage(false);
+    }
   }
   checkBoundary();
 }
 
 function endScreen() {
+  textSize(100);
+  let xSet = 0;
+  if(difficulty == "Normal") {
+    xSet = 30;
+  }
+  text(difficulty, 250-xSet, 100);
   textSize(80);
   text("Bugs Killed:", 100, 200);
   text(score, 575, 200);
@@ -77,16 +133,17 @@ function endScreen() {
         location.reload();
       }
     }
-}
+  }
 }
 
 function addBug() {
   let tempBug = new Bug(bugSprite, Math.floor(random(0, xMax)), Math.floor(random(60, yMax)));
-  setRandomMovement(tempBug, Math.floor(random(1, 5)));
+  setRandomMovement(tempBug);
   bugArray.push(tempBug);
 }
 
-function setRandomMovement(bug, movement) {
+function setRandomMovement(bug) {
+  let movement = Math.floor(random(1, 5));
   if (movement == 1) {
     bug.go(1, 0);
   }
@@ -159,6 +216,11 @@ class Bug {
     this.facing = 1;
     this.rotation = 0;
     this.dead = false;
+    this.deadX;
+    this.deadY;
+    this.deadRotation;
+    this.deadFacing;
+    this.change = false;
   }
 
   draw() {
@@ -179,13 +241,13 @@ class Bug {
         this.sx = (this.sx + 1) % 3;
       }
 
-      this.x += 2 * (this.horizontal * (1 + score * .03));
-      this.y += 2 * (this.vertical * (1 + score * .03));
+      this.x += 2 * (this.horizontal * (base_speed + score * increase_speed));
+      this.y += 2 * (this.vertical * (base_speed + score * increase_speed));
     }
     else {
-      translate(this.x, this.y);
-      rotate(this.rotation*PI/2);
-      scale(1, this.facing);
+      translate(this.deadX, this.deadY);
+      rotate(this.deadRotation*PI/2);
+      scale(1, this.deadFacing);
       image(this.spriteSheet, 0, 0, 40, 40, 0, 160, 160, 160);
     }
     pop();
@@ -198,6 +260,15 @@ class Bug {
   getY() {
     return this.y;
   }
+
+  hasChanged() {
+    return this.change;
+  }
+
+  setChanage(newChange) {
+    this.change = newChange;
+  }
+
   go(horizontal, vertical) {
     this.horizontal = horizontal;
     this.vertical = vertical;
@@ -223,5 +294,9 @@ class Bug {
 
   kill() {
     this.dead = true;
+    this.deadX = this.x;
+    this.deadY = this.y;
+    this.deadRotation = this.rotation;
+    this.deadFacing = this.facing;
   }
 }
